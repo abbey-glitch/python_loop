5a1525ae060ad25ffcc47bb80d13c8b9241024b0
abbey-glitch <abiodunonaolapi@gmail.com>
2023-05-11 17:15:25 +0100

    first

---
+++ loops.py
@@ -0,0 +1,21 @@
+# nested loops
+x = ["red", "blue", "green", "red"]
+length = len(x) - 1
+print(x[length])
+# profile = [1, 2, 3, 4]
+# print(profile[1])
+# for y in x:
+#     print(y)
+#    for d in profile:
+#       print(d)
+#       print(y)
+
+# the range function 
+# for x in range(20, 10,  -2):
+#     print(x)
+# while loop
+i=0
+while i<=10:
+    # to display the working of the loop product
+    print(i * 2)
+    i += 1
